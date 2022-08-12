a93594e3e4aaeeddeade8cacfe15ae2f1b90e892
Hrishi Singh <singh.123@iitj.ac.in>
2022-08-12 07:43:10 +0530

    added slides to header/core-team compl vers 1.0.5

--- src/Components/Events/Events.js
+++ src/Components/Events/Events.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import ComingSoon from '../ComingSoon/ComingSoon'
-console.log("hello events")
+// import ComingSoon from '../ComingSoon/ComingSoon'
+// console.log("hello events")
+import './Events.css'
 const Events = () => {
   return (
     <div>
-        <ComingSoon title="Events"/>
+        {/* <ComingSoon title="Events"/> */}
+        <section className='event-header'>
+        {/* <div class="box">
+                <h1>Events</h1>
+                <h4></h4>
+            </div> */}
+            <h1>Events</h1>
+          
+        </section>
     </div>
   )
 }
