2dc9cc573e2c97d7c627d29fbfc0eb1a29f7a34b
Shreya Pagaria <85084439+ShreyaPagaria@users.noreply.github.com>
2022-08-18 23:50:43 +0530

    Update About.js
    updated the About us text

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -9,7 +9,10 @@ const About = () => {
         <div className="content">
             
         </div>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi excepturi ab possimus labore iusto pariatur quae reiciendis ullam doloribus recusandae, unde culpa eligendi quaerat. Maxime perspiciatis eos laboriosam doloremque recusandae possimus? Quidem dignissimos dicta velit debitis distinctio cupiditate facilis sunt fuga explicabo repellat? Rem, maiores. Dolor laborum debitis voluptates consequuntu Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur hic nihil eos dolorum vero sapiente doloribus distinctio ad amet. Reiciendis nostrum ipsum accusantium eius quia ullam quis veritatis explicabo dignissimos, tempore vero officiis ex ab exercitationem facilis nihil, quo natus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos maiores aliquid ipsum illo asperiores nesciunt ducimus ut accusamus molestias, maxime deserunt voluptates. Animi harum voluptatem quia vitae nemo beatae laboriosam fuga velit placeat ratione officia accusamus necessitatibus facere, impedit quaerat nisi officiis odit modi ab commodi. Repellat ad inventore dolorem.
+        <p><Catalyst> is a one-day symposium organized by the Department of Chemical Engineering, IIT Jodhpur. It is an amalgamation of many academic components with 
+    a hint of fun such as competitive events, lectures, and workshops. We aim to provide a glimpse of the spirit of Chemical Engineering to the young undergraduate 
+    and postgraduate students from various corners of India. It will comprise lectures by <name of the lecturers>. The event is sponsored by industries like <company name>. 
+    We welcome you to the first Chemical Department event hosted at IIT Jodhpur. Check out the <events page >for more details!
         </p>
     </section>
     </>
