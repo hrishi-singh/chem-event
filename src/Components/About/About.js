721d9650e27ee8c1f521bfaf472e6b3b62c824c2
Shreya Pagaria <85084439+ShreyaPagaria@users.noreply.github.com>
2022-08-18 23:53:53 +0530

    Update About.js

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -9,10 +9,10 @@ const About = () => {
         <div className="content">
             
         </div>
-        <p><Catalyst> is a one-day symposium organized by the Department of Chemical Engineering, IIT Jodhpur. It is an amalgamation of many academic components with 
+        <p> {Catalyst} is a one-day symposium organized by the Department of Chemical Engineering, IIT Jodhpur. It is an amalgamation of many academic components with 
     a hint of fun such as competitive events, lectures, and workshops. We aim to provide a glimpse of the spirit of Chemical Engineering to the young undergraduate 
-    and postgraduate students from various corners of India. It will comprise lectures by <name of the lecturers>. The event is sponsored by industries like <company name>. 
-    We welcome you to the first Chemical Department event hosted at IIT Jodhpur. Check out the <events page >for more details!
+    and postgraduate students from various corners of India. It will comprise lectures by {name of the lecturers}. The event is sponsored by industries like {company name}. 
+    We welcome you to the first Chemical Department event hosted at IIT Jodhpur. Check out the {events page}for more details!
         </p>
     </section>
     </>
