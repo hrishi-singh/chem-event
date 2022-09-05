67c5af39d4bd5daef2a9ea54dc22df233aced0f7
Hrishi Singh <singh.123@iitj.ac.in>
2022-09-06 03:13:50 +0530

    bugs-fixes

--- src/Components/Events/Events2.js
+++ src/Components/Events/Events2.js
@@ -15,7 +15,6 @@ const Events2 = () => {
         <Cards title={"Lecture Series"} por={event_List.LectureSeries} btn={false} colm={4} tilt={false} className={'eventcard my-3'}/>
         <Cards title={"Poster Presentation"} por={event_List.PosterPresentation} btn={true} tilt={false} colm={6} className={'eventcard my-3'}/>
         <Cards title={"Events/Competitions"} por={event_List.EventsCompetitions} btn={false} tilt={false} colm={4} className={'eventcard my-3'}/>
-        <Cards title={"Prize Distribution"} por={event_List.ClosingRemarks} btn={false} colm={6} tilt={false} className={'eventcard my-3'}/>
         </section>
 
         
