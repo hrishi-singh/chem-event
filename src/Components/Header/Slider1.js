35c538ef0da6c70b1cbc6e1b6067cf2aa2a51ab1
Hrishi Singh <singh.123@iitj.ac.in>
2022-11-09 17:57:21 +0530

    update date 2.0.3.6

--- src/Components/Header/Slider1.js
+++ src/Components/Header/Slider1.js
@@ -17,14 +17,14 @@ const Slider1= (props) => {
           <h5> Theme: Sustainable Chemical Engineering <br/> </h5>
           <h6> Event Date: <br/></h6>
           {/* <h6> 14th January 2022<br/></h6> */}
-       <h5>14th January 2022.<br/></h5>
+       <h5>14th January 2023.<br/></h5>
        {/* <h5>Registration is temporarily Unavailable</h5> */}
        {currentUser.currentUser!=null?"":<>
        <h4>Free Registration for all.
         </h4> 
         <Link id='Register-btn' to='/registration'>Register Now!</Link>
         <h6> Last date to register: <br/>
-       <h4> 10th January 2022.</h4></h6> 
+       <h4> 10th January 2023.</h4></h6> 
         </>} 
         </div>      
     </section>
