62d723f50a69e827e9cb9985f384a8b23547e600
Hrishi Singh <singh.123@iitj.ac.in>
2022-12-19 19:28:04 +0530

    version 2.0.3.7 Added Sponsor

--- src/Components/Sponsors/Sponsors.js
+++ src/Components/Sponsors/Sponsors.js
@@ -1,29 +1,29 @@
 import React from 'react'
-// import CardGroup from 'react-bootstrap/CardGroup';
-// import Card from 'react-bootstrap/Card';
-// import Col from 'react-bootstrap/Col';
-// import Row from 'react-bootstrap/Row';
-import ComingSoon from '../ComingSoon/ComingSoon'
-// import sponsorList from './sponsors_List.json';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+// import ComingSoon from '../ComingSoon/ComingSoon'
+import sponsorList from './sponsors_List.json';
 import './Sponsors.css'
 const Sponsors = () => {
   return (
-    // <div className="container my-3">
-    // <h1>Our Sponsors</h1>
-    // <Row xs={1} md={3} className="g-4">
-    //   {sponsorList.sponsors.map((det) => (
-    //     <Col>
-    //       <Card>
-    //         <Card.Img variant="top" src={det.logo} />
-    //         <Card.Body>
+    <div className="container my-3" id="sponsorcontainer">
+    <h1>Our Sponsors</h1>
+    <Row xs={1} md={3} className="g-4">
+      {sponsorList.sponsors.map((det) => (
+        <Col>
+          <Card>
+            <Card.Img variant="top" src={det.logo} />
+            <Card.Body>
               
-    //         </Card.Body>
-    //       </Card>
-    //     </Col>
-    //   ))}
-    // </Row>
-    // </div>
-    <ComingSoon title={`Sponsors`}/>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+    </div>
+    // <ComingSoon title={`Sponsors`}/>
   )
 }
 export default Sponsors
