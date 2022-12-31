8b634b36cb85ce2068c101aa94458f3d51479c99
Hrishi Singh <singh.123@iitj.ac.in>
2022-12-31 13:18:05 +0530

    Events updated

--- src/Components/Events/Events2.js
+++ src/Components/Events/Events2.js
@@ -13,16 +13,18 @@ const Events2 = () => {
             <div className="box">
                 <h1>Events</h1>
             </div>
-            <h3>Total Prize Worth <img src="" alt="" />:</h3>
+            <h3>Total Prize Worth:</h3>
             <h2> 35K</h2>
             <a href='https://drive.google.com/file/d/1jV7vvCD97wP4hw0ZniwjdZi3xgKRwgpT/view?usp=sharing' target="#">View All Events Schedule</a>
+    <div className='Events__instruction'>
 
-            {currentUser.currentUser==null?<h5><br/>Kindly login to apply for Competitions / Presentation </h5>:""}
+            {currentUser.currentUser==null?<h5 ><br/>Kindly login to apply for Competitions / Presentation </h5>:<h5 ><br/>Fill the Respective Events Form Here</h5>}
+    </div>
         </section>
         <section id='eventslists'>
         <Cards title={"Aarambh"} por={event_List.AARAMBH} colm={6} tilt={false}  className={'eventcard my-3'}/>
         <Cards title={"Lecture Series"} por={event_List.Keynote} colm={12} tilt={false}  className={'eventcard my-3 eventTitle'}/>
-        <Cards title={""} por={event_List.LectureSeries} colm={6} tilt={false} className={'eventcard my-3 eventTitle'}/>
+        <Cards title={""} por={event_List.LectureSeries} colm={4} tilt={false} className={'eventcard my-3 eventTitle'}/>
         <Cards title={"Poster Presentation"} por={event_List.PosterPresentation} rulebookbtn={true} btn={currentUser.currentUser!=null?true:false} tilt={false} colm={6} className={'eventcard my-3 eventTitle'}/>
         <Cards title={"Events/Competitions"} por={event_List.EventsCompetitions} rulebookbtn={true} btn={currentUser.currentUser!=null?true:false} tilt={false} colm={6} className={'eventcard my-3 eventTitle'}/>
         <Cards title={"Informals"} por={event_List.Informals}rulebookbtn={true}  tilt={false} colm={12} className={'eventcard my-3 eventTitle'}/>
--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,11 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { useAuth } from '../contexts/AuthContext'
+import React, { useEffect, useState } from "react";
+import { useAuth } from "../contexts/AuthContext";
 import { getDatabase, ref, onValue } from "firebase/database";
-import './Profile.css'
+import "./Profile.css";
 
 function Profile() {
-
-
   const [userData, setUserData] = useState({});
   const currentUser = useAuth();
   const db = getDatabase();
@@ -14,85 +12,49 @@ function Profile() {
       //getting previously saved data
       // console.log({ SelectedCaseDetails });
       const getData = async () => {
-        const docRef = ref(
-          db,
-          'users/' + currentUser.currentUser.uid
-        );
+        const docRef = ref(db, "users/" + currentUser.currentUser.uid);
         //alert(docRef);
-        onValue(docRef, (snapshot) => {
-          const data = snapshot.val();
-          //alert(JSON.stringify(data));
-          setUserData(data);
-        }
-          , (errorObject) => {
-            console.log('The read failed: ' + errorObject.name);
+        onValue(
+          docRef,
+          (snapshot) => {
+            const data = snapshot.val();
+            //alert(JSON.stringify(data));
+            setUserData(data);
+          },
+          (errorObject) => {
+            console.log("The read failed: " + errorObject.name);
           }
         );
-
-
       };
       getData();
     } catch (error) {
       console.log({ error });
     }
-
-
   }, []);
 
   return (
-<div className='profilesection'> 
-  {/* <div className="container profilecon">
-    <div className="imgBx">
-    <img src="https://i.pinimg.com/564x/2a/18/5a/2a185a09ddedd2db1a3d34b6ce211557.jpg" alt="" />
-    </div>
-    <div className="details">
-      <div className="content">
+    <div className="profilesection">
+      <div class="profilecard" data-tilt>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-IpIPYsZ4B04ZB632wIBmK1zBg1LzGCXMoN_W4E16GBV3fIxynzR7mRoSRQU2qkTYUXk&usqp=CAU"
+          alt=""
+        />
         <div className="profile-box">
-
-      <p> Username : {userData.name}</p>
-      <p>Institute name : {userData.college}</p>
-            <p>Accomodation : {userData.accomodation}</p>
-            <p>Gender : {userData.gender}</p>
-            <p>Phone no. : {userData.phone}</p>
-            <p>Email : {userData.email}</p>
-            <p>Enrolled In : {userData.enrolled}</p>
+          <p>
+            {" "}
+            Name : <span>{userData ? userData.name : "Loading..."}</span>
+          </p>
+          <p>Institute name : {userData ? userData.college : "Loading..."}</p>
+          <p>Enrolled In : {userData ? userData.enrolled : "Loading..."}</p>
+          <p>Email : {userData ? userData.email : "Loading..."}</p>
+          <p>
+            Accomodation : {userData ? userData.accomodation : "Loading..."}
+          </p>
+          <p>Phone no. : {userData ? userData.phone : "Loading..."}</p>
+          <p>Gender : {userData ? userData.gender : "Loading..."}</p>
         </div>
       </div>
-
     </div>
-  </div> */}
- {/* <Card>
-        <Card.Img variant="top" src={"https://www.cumbria.ac.uk/media/staff-profile-images/staff_profile_-generic_350x350px.png"} />
-        <Card.Body>
-          <Card.Title>User Profile</Card.Title>
-          <Card.Text>
-            <p>Username : {userData.name}</p>
-            <p>Institute name : {userData.college}</p>
-            <p>Accomodation : {userData.accomodation}</p>
-            <p>Gender : {userData.gender}</p>
-            <p>Phone no. : {userData.phone}</p>
-            <p>Email : {userData.email}</p>
-            <p>Enrolled In : {userData.enrolled}</p>
-          </Card.Text>
-        </Card.Body>
-      </Card> */}
-       <div class="profilecard" data-tilt>
-                {/* <img src="" alt=""/> */}
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-IpIPYsZ4B04ZB632wIBmK1zBg1LzGCXMoN_W4E16GBV3fIxynzR7mRoSRQU2qkTYUXk&usqp=CAU" alt="" />
-                <div className="profile-box">
-      <p> Name : <spa>{userData? userData.name: 'Loading...'}</spa></p>
-      <p>Institute name : { userData?  userData.college: 'Loading...'}</p>
-            <p>Enrolled In : {userData? userData.enrolled: 'Loading...'}</p>
-            <p>Email : {userData? userData.email: 'Loading...'}</p>
-            <p>Accomodation : {userData? userData.accomodation: 'Loading...'}</p>
-            <p>Phone no. : {userData? userData.phone: 'Loading...'}</p>
-            <p>Gender : {userData? userData.gender: 'Loading...'}</p>
-        </div>
-
-            </div>
-</div>
-     
   );
 }
 export default Profile;
-
